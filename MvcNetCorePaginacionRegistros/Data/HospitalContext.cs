47de3058970500efd6f1b94485dd0cef393d4df5
DoryanAlexandro <doryanamt@gmail.com>
2025-03-05 14:11:46 +0100

    Add project files.

---
+++ MvcNetCorePaginacionRegistros/Data/HospitalContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcNetCorePaginacionRegistros.Models;
+
+namespace MvcNetCorePaginacionRegistros.Data
+{
+    public class HospitalContext: DbContext
+    {
+        public HospitalContext(DbContextOptions<HospitalContext> options)
+            :base(options){ }
+        public DbSet<Empleado> Empleados { get; set; }
+        public DbSet<Departamento> Departamentos  { get; set; }
+    }
+}
---
+++ MvcNetCorePaginacionRegistros/Repositories/RepositoryHospital.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcNetCorePaginacionRegistros.Data;
+using MvcNetCorePaginacionRegistros.Models;
+using System.Reflection.Metadata.Ecma335;
+
+namespace MvcNetCorePaginacionRegistros.Repositories
+{
+    public class RepositoryHospital
+    {
+        HospitalContext context;
+        public RepositoryHospital(HospitalContext context)
+        {
+            this.context = context;
+        }
+        
+        public async Task<List<Empleado>> GetEmpleadosAsync()
+        {
+            var consulta = from datos in this.context.Empleados
+                           select datos;
+            return await consulta.ToListAsync();
+        }
+        public async Task<List<Departamento>> GetDepartamentosAsync()
+        {
+            var departamentos = await this.context.Departamentos.ToListAsync();
+            return departamentos;
+        }
+        public  async Task<List<Empleado>> GetEmpleadosDepartamentoAsync
+            (int idDepartamento)
+        {
+            var empleados = this.context.Empleados
+                .Where(x => x.idDepartamento == idDepartamento);
+            if (empleados.Count() == 0)
+            {
+                return null;
+            }
+            else
+            {
+                return await empleados.ToListAsync();
+            }
+            
+        }
+    }
+}
